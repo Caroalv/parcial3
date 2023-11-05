224e8e60379ab64a9b77c5b6041561b6a8417269
Caroalv <alvaradocarolinad2002@gmail.com>
2023-11-05 10:36:07 -0600

    ultimos detalles

--- app/Models/CategoriaProducto.php
+++ app/Models/CategoriaProducto.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoriaProducto extends Model
 {
-    protected $table = 'categoria'; // Reemplaza 'nombre_de_la_tabla' por el nombre real de tu tabla
+    protected $table = 'categoria'; 
     protected $fillable = ['nombre', 'descripcion']; // Columnas que se pueden llenar en masa
 
 }
